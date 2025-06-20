c2d417922beae883e52bbe106239e9456fc684b9
Roma3577 <mularr37@gmail.com>
2025-06-20 16:57:23 +0300

    done5

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'auth',
   storage,
-  whitelist: ['token', 'user', 'isLoggedIn'],
+  whitelist: ['token',],
 };
 
 const rootReducer = combineReducers({
